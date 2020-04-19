52bec860b0d36e7c1028eee3578f04ce3fbee70b
Thom Bruce <thom@thombruce.com>
2020-04-20 00:28:01 +0100

    separate mailer templates

--- app/mailers/credible/confirmation_mailer.rb
+++ app/mailers/credible/confirmation_mailer.rb
@@ -1,18 +1,9 @@
-class Credible::UserMailer < ApplicationMailer
-  include Rails.application.routes.url_helpers
-
+class Credible::ConfirmationMailer < CredibleMailer
   def confirmation_email
     @app_name = Rails.application.class.module_parent_name
     @user = params[:user]
     @url  = root_url
     @confirmation_url = @url + 'confirm/' + @user.confirmation_token + '?email=' + @user.email
     mail(to: @user.email, subject: "Welcome to #{@app_name} | Please confirm your account")
-  end
-
-  def invitation_email
-    @app_name = Rails.application.class.module_parent_name
-    @user = params[:user]
-    @url  = root_url
-    mail(to: @user.email, subject: "You have been invited to #{@app_name}")
   end
 end
