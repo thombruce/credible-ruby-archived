30b08ea6227cffb463d30190dee3423b7d26cd72
Thom Bruce <thom@thombruce.com>
2020-04-22 19:13:26 +0100

    remove leading /

--- app/mailers/credible/invitation_mailer.rb
+++ app/mailers/credible/invitation_mailer.rb
@@ -3,7 +3,7 @@ class Credible::InvitationMailer < Credible::MailerBase
     @app_name = Rails.application.class.module_parent_name
     @user = params[:user]
     # TODO: Provide a means to have URL set by user
-    @invitation_url = main_app.root_url + '/confirm/' + @user.confirmation_token + '?email=' + @user.email
+    @invitation_url = main_app.root_url + 'confirm/' + @user.confirmation_token + '?email=' + @user.email
     mail(to: @user.email, subject: "You have been invited to #{@app_name}")
   end
 end
--- app/mailers/credible/reset_password_mailer.rb
+++ app/mailers/credible/reset_password_mailer.rb
@@ -3,7 +3,7 @@ class Credible::ResetPasswordMailer < Credible::MailerBase
     @app_name = Rails.application.class.module_parent_name
     @user = params[:user]
     # TODO: Provide a means to have URL set by user
-    @reset_password_url = main_app.root_url + '/confirm/' + @user.confirmation_token + '?email=' + @user.email
+    @reset_password_url = main_app.root_url + 'confirm/' + @user.confirmation_token + '?email=' + @user.email
     mail(to: @user.email, subject: "Please reset your #{@app_name} password")
   end
 end
