3657bfcbb9f88b83cb9f93f9bb2b332f40d47ae0
Thom Bruce <thom@thombruce.com>
2020-04-22 19:11:44 +0100

    employ main_app root_url as source of confirmation routing

--- app/mailers/credible/confirmation_mailer.rb
+++ app/mailers/credible/confirmation_mailer.rb
@@ -2,7 +2,8 @@ class Credible::ConfirmationMailer < Credible::MailerBase
   def confirmation_email
     @app_name = Rails.application.class.module_parent_name
     @user = params[:user]
-    @confirmation_url = confirmation_url(@user.confirmation_token, email: @user.email)
+    # TODO: Provide a means to have URL set by user
+    @confirmation_url = main_app.root_url + '/confirm/' + @user.confirmation_token + '?email=' + @user.email
     mail(to: @user.email, subject: "Welcome to #{@app_name} | Please confirm your account")
   end
 end
--- app/mailers/credible/reset_password_mailer.rb
+++ app/mailers/credible/reset_password_mailer.rb
@@ -2,7 +2,8 @@ class Credible::ResetPasswordMailer < Credible::MailerBase
   def reset_password_email
     @app_name = Rails.application.class.module_parent_name
     @user = params[:user]
-    @reset_password_url = confirmation_url(@user.confirmation_token, email: @user.email)
+    # TODO: Provide a means to have URL set by user
+    @reset_password_url = main_app.root_url + '/confirm/' + @user.confirmation_token + '?email=' + @user.email
     mail(to: @user.email, subject: "Please reset your #{@app_name} password")
   end
 end
