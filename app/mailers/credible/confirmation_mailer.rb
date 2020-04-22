9efb293c5f0ca14f776141bf597bbeadbaa97e97
Thom Bruce <thom@thombruce.com>
2020-04-22 16:20:14 +0100

    rename confirm route

--- app/mailers/credible/confirmation_mailer.rb
+++ app/mailers/credible/confirmation_mailer.rb
@@ -2,8 +2,7 @@ class Credible::ConfirmationMailer < CredibleMailer
   def confirmation_email
     @app_name = Rails.application.class.module_parent_name
     @user = params[:user]
-    @url  = root_url
-    @confirmation_url = @url + 'confirm/' + @user.confirmation_token + '?email=' + @user.email
+    @confirmation_url = confirm_url(@user.confirmation_token, email: @user.email)
     mail(to: @user.email, subject: "Welcome to #{@app_name} | Please confirm your account")
   end
 end
--- app/mailers/credible/reset_password_mailer.rb
+++ app/mailers/credible/reset_password_mailer.rb
@@ -2,8 +2,7 @@ class Credible::ResetPasswordMailer < CredibleMailer
   def reset_password_email
     @app_name = Rails.application.class.module_parent_name
     @user = params[:user]
-    @url  = root_url
-    @reset_password_url = @url + 'confirm/' + @user.confirmation_token + '?email=' + @user.email
+    @reset_password_url = confirm_url(@user.confirmation_token, email: @user.email)
     mail(to: @user.email, subject: "Please reset your #{@app_name} password")
   end
 end
